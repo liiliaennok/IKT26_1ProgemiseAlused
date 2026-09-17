c2b7f820f315bfa025dee719052de2c2d39661f8
Opilane Techno+TLN <opilane@tthk.ee>
2026-09-17 10:45:45 +0300

    IfAndElseNesting3

---
+++ IfAndElseNesting/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace IfAndElseNesting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teha üks if ja else nestimine iseseisvalt");
+            Console.WriteLine("Teise else if-i sisse panna if and else nestimine");
+            //konsool küsib numbrit
+            //if võrdub 12
+            //else if ja siia sisse omakorda teha if ning else. else if tingimus on, et
+            //muutuja on suurem, kui 20
+            //else ja seal väljastab konsool teksti: Mingid kahtlased väärtused
+            Console.WriteLine("sisesta number");
+
+            string input = Console.ReadLine();
+
+            if (int.TryParse(input, out int number))
+            
+            if (number == 12)
+            {
+                if (number == 12)
+                {
+                    Console.WriteLine("õige vastus");
+                }
+                else
+                {
+                    Console.WriteLine("Vastus oli 12");
+                }
+            }
+            else if (number > 20)
+
+                Console.WriteLine("Mingid kahtlased väärtused");
+            {
+
+            }
+        }
+    }
+}
